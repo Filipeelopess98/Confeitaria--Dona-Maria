cf5bf65d92c14c612de2057871743281a6aa8c68
Filipeelopess98 <filipee.lopess98@gmail.com>
2024-04-29 21:48:58 -0300

    file

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ menu.addEventListener("click", () => {
     menu.classList.toggle('ativo');
     navMenu.classList.toggle('ativo');
 })
+
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+});
+
